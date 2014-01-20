a4b2bdb03053642a09ca0acc8cdb9c61e3807615
Daniel Levenson <dleve123@gmail.com>
2014-01-20 01:46:42 -0500

    Add and configure middleman-deploy for gh-pages

--- config.rb
+++ config.rb
@@ -51,6 +51,11 @@ set :js_dir, 'js'
 
 set :images_dir, 'img'
 
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
